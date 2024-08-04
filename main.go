5e2d56f906dbc1dd2397e220b4802b117ae7e268
Muhammad Saim <muhammadsaim494@gmail.com>
2024-08-05 01:12:13 +0500

    add basic routing functions

--- main.go
+++ main.go
@@ -10,11 +10,24 @@ func home(w http.ResponseWriter, r *http.Request){
 	w.Write([]byte("Hello from Snippetbox"))
 }
 
+// add snippetView hadnle function
+func snippetView(w http.ResponseWriter, r *http.Request){
+	w.Write([]byte("Display a specific snippet"))
+}
+
+// add snippetCreate hadnle function
+func snippetCreate(w http.ResponseWriter, r *http.Request){
+	w.Write([]byte("Display a form for creating a new snippet"))
+}
+
+
 func main(){
 	// use the http.NewServerMux() for creating the servermux
 	// register the home function as the handler for the root url
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
+	mux.HandleFunc("/snippet/view", snippetView)
+	mux.HandleFunc("/snippet/create", snippetCreate)
 
 	// printing the log message
 	log.Print("Starting server on :4000")
